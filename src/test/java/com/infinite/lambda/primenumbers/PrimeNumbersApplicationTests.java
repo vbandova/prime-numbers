53e81f53eae2ba06ee0041229da877a61c6556cc
Vesela Bandova <v.bandova@gmail.com>
2020-11-07 16:49:02 +0200

    Initial commit

---
+++ src/test/java/com/infinite/lambda/primenumbers/PrimeNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinite.lambda.primenumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
